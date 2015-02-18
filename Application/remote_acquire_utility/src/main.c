e97a5af9208b4049676b27cd5ca35e13f9ae30f5
Brandon Kinman <bkinman@vysk.com>
2015-02-17 21:34:12 -0800

    Pushed the peg forward here a little bit today. It seems that if a JSON "params" can only contain a list of name/value pairs where the value is a number. Passing a "params" in with a name/value pair where the value is anything other than a number will cause nginx to crash terribly. As such, the remote ip address must be passed in a parameter at a time using numbers.

--- Application/remote_acquire_utility/src/main.c
+++ Application/remote_acquire_utility/src/main.c
@@ -11,37 +11,52 @@
 #include "version.h"
 
 
+#define LOG(args...) fprintf(stderr, args)
+
+
 const char *rp_app_desc(void)
 {
+	LOG("%s: hit\n",__func__);
     return (const char *)"Red Pitaya Remote Acquisition Utility.\n";
 }
 
 int rp_app_init(void)
 {
-    fprintf(stderr, "Loading Remote Acquisition Utility version %s-%s.\n", VERSION_STR, REVISION_STR);
+	LOG("Loading Remote Acquisition Utility.\n");
 
     return 0;
 }
 
 int rp_app_exit(void)
 {
-    fprintf(stderr, "Unloading Remote Acuiqisition Utility version %s-%s.\n", VERSION_STR, REVISION_STR);
+	LOG("Unloading Remote Acquisition Utility.\n");
 
     return 0;
 }
 
 int rp_set_params(rp_app_params_t *p, int len)
 {
+	LOG("%s was called.\n",__func__);
+	LOG("Received %d params.\n",len);
+
+	int i;
+	for(i=0;i<len;i++)
+	{
+		LOG("Name: %s\n",p[i].name);
+	}
+
     return 0;
 }
 
 /* Returned vector must be free'd externally! */
 int rp_get_params(rp_app_params_t **p)
 {
+	LOG("%s was called.\n",__func__);
 	return 0;
 }
 
 int rp_get_signals(float ***s, int *sig_num, int *sig_len)
 {
+	LOG("%s was called.\n",__func__);
     return 0;
 }
